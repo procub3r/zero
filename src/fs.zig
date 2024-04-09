973798a654013323fe8f9236409085e49ed141cf
procub3r <procub3r@proton.me>
2024-04-09 19:01:18 +0530

    Create folders that don't exist + refactor

--- src/fs.zig
+++ src/fs.zig
@@ -1,5 +1,24 @@
 const std = @import("std");
 
+pub fn createFileMakePath(file_path: []const u8) !std.fs.File {
+    // print error message on error
+    errdefer std.log.err("couldn't create file {s}", .{file_path});
+
+    // create the file if it doesn't exist and open it (for writing by default)
+    const file = std.fs.cwd().createFile(file_path, .{}) catch mkdir: {
+        // if there is an error, it is most likely because the post directory doesn't exist.
+        // figure out the post directory's name
+        const dir_path = std.fs.path.dirname(file_path).?;
+
+        // create the post directory
+        try std.fs.cwd().makePath(dir_path);
+
+        // try to create the file again. if it fails, report error and give up
+        break :mkdir try std.fs.cwd().createFile(file_path, .{});
+    };
+    return file;
+}
+
 pub fn readFile(alloc: std.mem.Allocator, dir: std.fs.Dir, path: []const u8) ![]const u8 {
     const file = dir.openFile(path, .{ .mode = .read_only }) catch |err| {
         std.log.err("couldn't open file {s} for reading", .{path});
--- src/post.zig
+++ src/post.zig
@@ -4,20 +4,11 @@ const md = @import("md.zig");
 
 pub fn render(alloc: std.mem.Allocator, source_dir: std.fs.Dir, source_path: std.fs.Dir.Walker.WalkerEntry) !void {
     // determine path to the post rendered from the source file
-    var post_dir_path = source_path.path[0 .. source_path.path.len - source_path.basename.len];
-    post_dir_path = if (post_dir_path.len != 0) post_dir_path else "./"; // set to cwd if it's empty
-    const post_path = try std.mem.concat(alloc, u8, &.{ source_path.basename[0 .. source_path.basename.len - 2], "html" });
-    std.log.info("rendering post {s}{s}", .{ post_dir_path, post_path });
-
-    // create the post directory if it doesn't exist, and open it
-    var post_dir = try std.fs.cwd().makeOpenPath(post_dir_path, .{});
-    defer post_dir.close();
+    const post_path = try std.mem.concat(alloc, u8, &.{ source_path.path[0 .. source_path.path.len - 2], "html" });
+    std.log.info("rendering post {s}", .{post_path});
 
     // create the post file if it doesn't exist, and open it for writing
-    const post_file = post_dir.createFile(post_path, .{}) catch {
-        std.log.err("couldn't open file {s}{s} for writing", .{ post_dir_path, post_path });
-        return;
-    };
+    const post_file = try fs.createFileMakePath(post_path);
     defer post_file.close();
 
     // create a buffered writer to write to the post file
@@ -30,5 +21,5 @@ pub fn render(alloc: std.mem.Allocator, source_dir: std.fs.Dir, source_path: std
     // render the raw md from source to post for debug purposes
     try md.toHtml(post_writer, source);
 
-    std.log.info(" rendered post {s}{s}\n", .{ post_dir_path, post_path });
+    std.log.info(" rendered post {s}\n", .{post_path});
 }
